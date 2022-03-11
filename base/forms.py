80d37a492cc778ed92e0230b0db95e00869d04f7
Brijesh Varsani <varsani340@gmail.com>
2022-03-11 14:13:40 +0100

    update

--- base/forms.py
+++ base/forms.py
@@ -8,42 +8,57 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserCreateForm(UserCreationForm):
-    
+
     class Meta:
         model = User
-        fields = ['name', 'username', 'email', 'bio', 'groupId', 'residenceAddressId']
+        fields = ['name', 'username', 'email',
+                  'bio', 'groupId', 'residenceAddressId']
 
 
 class IssueCreateForm(ModelForm):
 
-    class Meta: 
+    class Meta:
         model = Issue
         fields = '__all__'
-        exclude = ['assigner', 'assignee', 'submitter' ]
-        # ass exclude in view.py before save/commit  
+        exclude = ['assigner', 'assignee', 'submitter']
+        # ass exclude in view.py before save/commit
 
+
+class IssueCategoryCreateForm(ModelForm):
+
+    class Meta:
+        model = IssueCategory
+        fields = '__all__'
 
 
 class CommentsCreateForm(ModelForm):
 
-    class Meta: 
+    class Meta:
         model = Comments
         fields = ['messageText']
-        
 
 
 class AddressCreateForm(ModelForm):
 
-    class Meta: 
+    class Meta:
         model = Address
         fields = '__all__'
-        
 
 
 class PropertyCreateForm(ModelForm):
 
-    class Meta: 
+    class Meta:
         model = Property
         fields = '__all__'
         exclude = ['ownerId']
         # add ownerId from view
+
+
+
+class IssueUpdateForm(ModelForm):
+
+    class Meta:
+        model = Issue
+        fields = ['assigner', 'assignee']
+        # ass exclude in view.py before save/commit
+
--- base/views.py
+++ base/views.py
@@ -1,8 +1,9 @@
+from multiprocessing import context
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import User
+from .models import *
 from .forms import *
 
 
@@ -44,7 +45,10 @@ def logoutPage(request):
 @login_required(login_url="loginPage")
 def home(request):
     user = request.user
-    context = {'user': user}
+    issues = Issue.objects.all()
+
+    context = {'user': user,
+    'issues': issues}
     return render(request, "base/home.html", context)
 
 
@@ -90,3 +94,68 @@ def propertyRegisterPage(request):
         form = PropertyCreateForm()
 
     return render(request, 'base/forms/property_register_form.html', {'form': form})
+
+
+def issueCategoryRegisterPage(request):
+
+    if request.method == 'POST':
+        form = IssueCategoryCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = IssueCategoryCreateForm()
+
+    return render(request, 'base/forms/issue_category_register_form.html', {'form': form})
+
+
+
+def issueRegisterPage(request):
+    user = request.user.id
+
+    if request.method == 'POST':
+        form = IssueCreateForm(request.POST)
+        if form.is_valid():
+            addSubmiterId = form.save(commit=False)
+            addSubmiterId.submitter_id = user
+            addSubmiterId.save()
+            form.save_m2m()
+            return redirect('home')
+    else:
+        form = IssueCreateForm()
+
+    return render(request, 'base/forms/issue_create_form.html', {'form': form})
+
+
+
+def issueUpdatePage(request):
+    user = request.user.id
+
+    if request.method == 'POST':
+        form = IssueCreateForm(request.POST)
+        if form.is_valid():
+            addSubmiterId = form.save(commit=False)
+            addSubmiterId.submitter_id = user
+            addSubmiterId.save()
+            form.save_m2m()
+            return redirect('home')
+    else:
+        form = IssueCreateForm()
+
+    return render(request, 'base/forms/issue_create_form.html', {'form': form})
+
+
+def updateManagerIssue(request,pk):
+    issues = Issue.objects.all()
+    print(pk)
+    if request.method == 'POST':
+        form = IssueUpdateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = IssueUpdateForm()
+
+    context = {'issues': issues,
+                'form': form}
+    return render(request, 'base/home.html', context)
